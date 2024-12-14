3241f04de5cec78b4fcb0574e501839a1b52aa26
moonjiho11 <giho0214@mail.ulsan.ac.kr>
2024-12-14 16:28:01 +0900

    Update views.py

--- SW_project-main/Real_Final/weather_project/weather_app/views.py
+++ SW_project-main/Real_Final/weather_project/weather_app/views.py
@@ -64,10 +64,10 @@ def login_view(request):
         username = request.POST['username']
         password = request.POST['password']
         user = authenticate(request, username=username, password=password)
-        if request.user.is_authenticated:
-            return redirect('weather')
+       
         if user is not None:
             login(request, user)
+            reurn redirect('weather')
           
         else:
             messages.error(request, "로그인 정보가 올바르지 않습니다.")

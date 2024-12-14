9ff464565308762d213ac36f2c67d77eaf368ec2
moonjiho11 <giho0214@mail.ulsan.ac.kr>
2024-12-14 12:54:15 +0900

    Update settings.py

--- SW_project-main/Real_Final/weather_project/weather_project/settings.py
+++ SW_project-main/Real_Final/weather_project/weather_project/settings.py
@@ -25,11 +25,11 @@ SECRET_KEY = 'django-insecure-o-j4mwdn3d4xahx0r8d27+bq17t#35&=25y79%!v5g+8dz%dmf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-
 ALLOWED_HOSTS = ['*']
-CSRF_TRUSTED_ORIGINS = [ 'https://port-0-online-repo-m4mn6khjc2c41aa0.sel4.cloudtype.app' ]
 
-
+CSRF_TRUSTED_ORIGINS = [
+    'https://port-0-online-repo-m4mn6khjc2c41aa0.sel4.cloudtype.app'
+]
 
 # Application definition
 
@@ -73,7 +73,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'weather_project.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
@@ -83,7 +82,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -103,7 +101,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -114,7 +111,6 @@ TIME_ZONE = 'Asia/Seoul'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
